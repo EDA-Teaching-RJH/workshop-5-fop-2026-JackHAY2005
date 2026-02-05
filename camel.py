f26836cf9c4916d8603f5eaa49674997ad8a23b0
JackHAY2005 <jch12@kent.ac.uk>
2026-02-05 10:04:59 +0000

    Coding

--- camel.py
+++ camel.py
@@ -1,1 +1,10 @@
-# Enter your code here
+import re
+def main():
+    camel = input("What is your first and last name? ")
+
+    for i in range(len(camel)):
+        if camel[i].isupper():
+            print("_" + camel[i].lower(), end="")
+        else:
+            print(camel[i], end="")
+main()
--- coffee.py
+++ coffee.py
@@ -1,1 +1,31 @@
-#Your code goes here
+def main():
+    total = 0
+    while total < 75:
+        input_coins = int(input("Please enter coins. "))
+        if total == 75:
+            print("You have entered 75p. You can buy a coffee.")
+        elif input_coins == 0:
+            print("You have entered 0p. Please enter coins.")
+        elif input_coins == 5:
+            total += input_coins
+            print("You have entered 5p.")
+        elif input_coins == 10:
+            total += input_coins
+            print("You have entered 10p.")
+        elif input_coins == 20:
+            total += input_coins
+            print("You have entered 20p.")
+        elif input_coins == 50:
+            total += input_coins
+            print("You have entered 50p.")
+        else:
+            print("Invalid coin. Please enter 5p, 10p, 20p, or 50p.")
+        amount_owed = 75 - total
+        if amount_owed > 0:
+            print("You still need " + str(amount_owed) + "p.")
+        else:
+            change = total - 75
+            print("Here is your coffee. Your change is " + str(change) + "p.")
+
+main()
+
